e45f0c78bc53695c1d8f16c7aa6ae9675ad8ac71
Nathan Bailey <nathanbaileyw@gmail.com>
2024-06-20 20:36:55 +0100

    Adds initial files

---
+++ image_generator.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from display import display
+
+class ImageGenerator(keras.callbacks.Callback):
+    def __init__(self, num_img: int) -> None:
+        self.num_img = num_img
+    
+    def sample_from(self, probs: tf.Tensor, temperature: float) -> np.ndarray:
+        probs = probs ** (1 / temperature)
+        probs = probs / np.sum(probs)
+        #Choose a random number using the distribution
+        return np.random.choice(len(probs), p=probs)
+
+    def generate(self, temperature: float, pixel_levels: int) -> np.ndarray:
+        generated_images = np.zeros(shape=(self.num_img,) + self.model.input_shape[1:])
+        _, rows, cols, channels = generated_images.shape
+        for row in range(rows):
+            for col in range(cols):
+                for channel in range(channels):
+                    #Predict pixels one by one using the previously predicted pixels
+                    probs = self.model.predict(generated_images, verbose=0)[:, row, col, :]
+                    generated_images[:, row, col, channel] = [
+                        self.sample_from(x, temperature) for x in probs
+                    ]
+                    generated_images[:, row, col, channel] /= pixel_levels
+        return generated_images
+    
+    def on_epoch_end(self, epoch, pixel_levels: int, logs=None) -> None:
+        generated_images = self.generate(temperature=1.0, pixel_levels=pixel_levels)
+        display(
+            generated_images,
+            save_to=f"./output/generated_img_{epoch}.png",
+        )
+
+        
+
+    
---
+++ model_building_blocks.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from typing import Literal
+from typing import Any
+
+
+class MaskedConv2D(keras.layers.Layer):
+    def __init__(self, mask_type: Literal["A", "B"], **kwargs: Any) -> None:
+        super().__init__()
+        self.mask_type = mask_type
+        self.conv_layer = keras.layers.Conv2D(**kwargs)
+    
+    def build(self, input_shape: tuple[int]) -> None:
+        self.conv_layer.build(input_shape)
+        #Kernel shape is F1xF2xDepthxNum Filters
+        kernel_shape = self.conv_layer.kernel.get_shape()
+        self.mask = np.zeros(shape=kernel_shape)
+        #Set half - 1 rows to 1.0s
+        self.mask[:kernel_shape[0] // 2, ...] = 1.0
+        #Set the half rows tp 1.0 up to the middle - 1 column
+        self.mask[kernel_shape[0] // 2, :kernel_shape[1] // 2, ...] = 1.0
+        #If the mask type is B, set the middle value to 1.0
+        if self.mask_type == "B":
+            self.mask[kernel_shape[0] // 2, kernel_shape[1] // 2, ...] = 1.0
+    
+    def call(self, input: tf.Tensor) -> tf.Tensor:
+        self.conv.kernel.assign(self.conv.kernel*self.mask)
+        return self.conv(input)
+
+
+
+def ResidualBlock(keras.layers.Layer):
+    def __init__(self, filters: int) -> None:
+        super().__init__()
+        self.conv_layer_1 = keras.layers.Conv2D(filters = filters // 2, kernel_size=1, actiavtion="relu")
+        self.pixel_conv = MaskedConv2D(mask_type="B", filters = filters // 2, kernel_size=3, actiavtion="relu", padding="same")
+        self.conv_layer_2 = keras.layers.Conv2D(filters=filters, kernel_size=1, activation="relu")
+    
+    def call(self, input: tf.Tensor) -> tf.Tensor:
+        """
+        Spatial or depth dimensions will not change between input and output.
+        So no need to downsample anything, we just add input and output.
+        """
+        x = self.conv_layer_1(input)
+        x = self.pixel_conv(x)
+        x = self.conv_layer_2(x)
+        return (input + x)
+
